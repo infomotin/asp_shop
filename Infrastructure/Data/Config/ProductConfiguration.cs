6f8739a30ae32a33d968fd0932bd204478aec1ac
Abdul Motin <motin.mmk.jr@gmail.com>
2020-10-29 17:33:26 +0600

    Initial Commit

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder){
+            throw new System.NotImplementedException();
+        }
+    }
+}
