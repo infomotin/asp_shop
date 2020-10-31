27580edfd3671bddf8d9518fafdaa4b7b46c61a9
Abdul Motin <motin.mmk.jr@gmail.com>
2020-10-31 17:12:56 +0600

    first commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,13 +11,23 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async  Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope()){
                 var services = scope.ServiceProvider;
                 var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try{
+                    var context =services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                }catch(Exception ex){
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex," An Error");
+                }
+
             }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
