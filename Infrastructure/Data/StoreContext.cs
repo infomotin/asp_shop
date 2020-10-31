bc2dffbea9551cff6818466027c377ef3c398030
Abdul Motin <motin.mmk.jr@gmail.com>
2020-10-31 15:47:26 +0600

    first commit

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using System.Reflection;
+
 namespace Infrastructure.Data
 {
     public class StoreContext: DbContext
@@ -10,5 +12,11 @@ namespace Infrastructure.Data
         public DbSet<Product> Products {get;set;}
         public DbSet<ProductBrand> ProductBrand {get;set;}
         public DbSet<ProductType> ProductType {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
