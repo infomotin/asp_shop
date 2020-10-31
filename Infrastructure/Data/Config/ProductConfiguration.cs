2b0181bff6b6c51c3dcc29e3c73cf098303e2c3d
Abdul Motin <motin.mmk.jr@gmail.com>
2020-10-31 15:38:15 +0600

    first commit

--- Infrastructure/Data/Config/ProductConfiguration.cs
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -6,7 +6,13 @@ namespace Infrastructure.Data.Config
     public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
         public void Configure(EntityTypeBuilder<Product> builder){
-            throw new System.NotImplementedException();
+            builder.Property(p =>p.id).IsRequired();
+            builder.Property(p =>p.Name).IsRequired().HasMaxLength(150);
+            builder.Property(p =>p.Description).IsRequired().HasMaxLength(180);
+            builder.Property(p =>p.Price).HasColumnType("decimal(18.2)");
+            builder.Property(p =>p.PictureUrl).IsRequired();
+            builder.HasOne(b =>b.ProductBrand).WithMany().HasForeignKey(p =>p.ProductBrandId);
+            builder.HasOne(t =>t.ProductType).WithMany().HasForeignKey(p =>p.ProductTypeId);
         }
     }
 }
