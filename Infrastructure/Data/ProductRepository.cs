b8dcbc07ea7c0895e7959c64a30859892d2dc6f3
Abdul Motin <motin.mmk.jr@gmail.com>
2020-10-31 18:38:40 +0600

    first commit

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -18,10 +18,10 @@ namespace Infrastructure.Data
         }
 
         public async Task<Product> GetProductByIdAsync(int id){
-            return await _context.Products.FindAsync(id);
+            return await _context.Products.Include(p =>p.ProductBrand).Include(p =>p.ProductType).FirstOrDefaultAsync(p =>p.id == id);
         }
         public async Task<IReadOnlyList<Product>> GetProductsAsync(){
-            return await _context.Products.ToListAsync();
+            return await _context.Products.Include(p =>p.ProductBrand).Include(p =>p.ProductType).ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetTypesAsync()
