08bc78ce56d4deac2d6603eef67967c23a25966c
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-01 11:38:29 +0600

    GenericRepository

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -7,6 +7,12 @@ namespace Infrastructure.Data
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
         public Task<T> GetByIdAsync(int id)
         {
             throw new System.NotImplementedException();
