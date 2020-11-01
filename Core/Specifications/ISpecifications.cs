95dac33c08ba03603eccda7360c52f151770d31b
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-01 13:00:03 +0600

    GenericRepository

---
+++ Core/Specifications/ISpecifications.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecifications<T> 
+    {
+         //this is generic repository anti patten 
+
+        //  Sentext :Exprations <Func<T,bool>Query>>
+
+        Expression<Func<T,bool>> Criteria {get;}
+        List<Expression<Func<T,object>>> include{get;}
+    }
+}
