6a2a5f8c0db872a280bb6cde82a67fe787261ab2
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-02 15:18:43 +0600

    BuggyControl app

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,13 +1,44 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class BuggyController :BaseApiController
+    public class BuggyController : BaseApiController
     {
-        [HttpGet("notFound")]
-        public ActionResult GetNotFoundRequest(){
-            
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
         }
-        
+
+        [HttpGet("notFound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var testProduct = _context.Products.Find(-42);
+            if (testProduct == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {   var testProduct = _context.Products.Find(-42);
+            var resultP = testProduct.ToString();
+            return Ok();
+        }
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+         [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+
+
     }
 }
