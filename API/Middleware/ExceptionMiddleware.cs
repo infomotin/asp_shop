3fbc4179e089c421898ef68265d124ac06be5847
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-03 11:02:36 +0600

    BuggyControl ExceptionMiddleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+    }
+}
