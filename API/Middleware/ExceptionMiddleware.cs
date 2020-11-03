920764272173ce33e833693b10a1346cc18d4236
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-03 11:22:08 +0600

    BuggyControl ExceptionMiddleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System.Net;
+
 namespace API.Middleware
 {
     public class ExceptionMiddleware
@@ -13,6 +16,21 @@ namespace API.Middleware
             _env = env;
             _logger = logger;
             _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context){
+            try
+            {
+                await _next(context);
+                
+            }
+            catch (System.Exception ex)
+            {
+                
+                _logger.LogError(ex,ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                
+            }
         }
     }
 }
