d86452495d85bef6da979a0ded551c3c8e2e4966
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-03 12:07:03 +0600

    BuggyControl ExceptionMiddleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -3,11 +3,15 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 using System.Net;
+using API.Errors;
 
 namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
+        //RequestDelegate function that can process an HTTP request.
+        // Returns:
+        // A task that represents the completion of request processing.
         private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
@@ -17,6 +21,8 @@ namespace API.Middleware
             _logger = logger;
             _next = next;
         }
+        //HttpContext Encapsulates all HTTP-specific information about an individual HTTP request. 
+        //Awaited task returns no value
         public async Task InvokeAsync(HttpContext context){
             try
             {
@@ -29,6 +35,8 @@ namespace API.Middleware
                 _logger.LogError(ex,ex.Message);
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                //ApiException is an have peremitter with value initialize ing 
+                var response = _env.IsDevelopment() ? new ApiException((int)HttpStatusCode.InternalServerError,ex.Message,ex.StackTrace.ToString());
                 
             }
         }
